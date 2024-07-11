f7c245094be849a0b9b00f02838625552d01cdb8
kalair22 <kalaivaniravi2208@gmail.com>
2024-07-11 15:18:14 +0530

    update

--- job-server/index.js
+++ job-server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mernjob.cmk1ku9.mongodb.net/?retryWrites=true&w=majority`
+const uri = "mongodb+srv://kalaivaniravi2208:KJ4mQpm79nU8sxrZ@mernjob.cmk1ku9.mongodb.net/"
 if (!uri) {
   console.error('MongoDB URI is not set. Please check your environment variables.');
   process.exit(1);
