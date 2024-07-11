751ab110122b05201885c5780f66e6cb58cfece8
kalair22 <kalaivaniravi2208@gmail.com>
2024-07-11 15:59:45 +0530

    update

--- job-client/src/sidebar/Sidebar.jsx
+++ job-client/src/sidebar/Sidebar.jsx
@@ -1,24 +1,19 @@
-import React from 'react'
-import Location from './Location'
-import Salary from './Salary'
-import JobPosting from './JobPosting'
-import WorkExperience from './workExperience'
-import Employment from './Employment'
+import React from "react";
+import Location from "./Location";
+import Salary from "./Salary";
+import JobPosting from "./JobPosting";
+import Employment from "./Employment";
 
-
-
-const Sidebar = ({handleChange, handleClick}) => {
+const Sidebar = ({ handleChange, handleClick }) => {
   return (
-    <div className='space-y-5'>
-      <h3 className='text-l font-bold mb-2'>Filters</h3>
-
-      <Location handleChange={handleChange}/>
-      <Salary handleChange={handleChange} handleClick={handleClick}/>
-      <JobPosting handleChange={handleChange}/>
-      <WorkExperience handleChange={handleChange}/>
-      <Employment handleChange={handleChange}/>
+    <div className="space-y-5">
+      <h3 className="text-l font-bold mb-2">Filters</h3>
+      <Location handleChange={handleChange} />
+      <Salary handleChange={handleChange} handleClick={handleClick} />
+      <JobPosting handleChange={handleChange} />
+      <Employment handleChange={handleChange} />
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
